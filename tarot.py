11a3f36ecc6498e8eab2a01985c7dce742831c1a
sjskoko <77188135+sjskoko@users.noreply.github.com>
2022-03-04 12:21:58 +0900

    타로 뽑기 코드 추가

--- tarot.py
+++ tarot.py
@@ -6,9 +6,14 @@ for i in range(10000):
 
 
 
-def card_pick(num=1):
+def card_pick(num=1, seed=None):
+    if seed:
+        np.random.seed(seed)
     suit_list = ['wand', 'cup', 'sword', 'pentacle']
     minor_list = [str(i) for i in range(1, 11)] + ['page', 'knight', 'queen', 'king']
+
+    result = [] 
+
     for i in range(num):
         card = np.random.randint(0, 78)
 
@@ -17,10 +22,12 @@ def card_pick(num=1):
             suit = suit_list[(card-22)//14]
             card_number = minor_list[(card-22)%14]
 
-            return suit + '_' + card_number
+            result.append(suit + '_' + card_number)
         else:
             
-            return 'major number ' + str(card)
+            result.append('major number ' + str(card))
+    
+    return result
 
 
 deck = sorted(set([card_pick() for _ in range(1000000)]))
