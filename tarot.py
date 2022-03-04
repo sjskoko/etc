25931eebf2fdb6d08a2f40a7bf44373a325f51d2
sjskoko <77188135+sjskoko@users.noreply.github.com>
2022-03-04 12:49:43 +0900

    타로 뽑기 코드

--- tarot.py
+++ tarot.py
@@ -1,21 +1,18 @@
 import numpy as np
 
-temp = []
-for i in range(10000):
-    temp.append(np.random.randint(0, 78))
 
 
 
-def card_pick(num=1, seed=None):
-    if seed:
-        np.random.seed(seed)
+def card_pick(num=1, shuffle=1):
+
     suit_list = ['wand', 'cup', 'sword', 'pentacle']
     minor_list = [str(i) for i in range(1, 11)] + ['page', 'knight', 'queen', 'king']
 
     result = [] 
 
     for i in range(num):
-        card = np.random.randint(0, 78)
+        for _ in range(shuffle):
+            card = np.random.randint(0, 78)
 
         if card >= 22:
             sign = 'miner'
@@ -29,7 +26,4 @@ def card_pick(num=1, seed=None):
     
     return result
 
-
-deck = sorted(set([card_pick() for _ in range(1000000)]))
-
-card_pick()
+card_pick(2, 39)
